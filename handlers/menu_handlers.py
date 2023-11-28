1229056e100aa4d2d9ef2bb9adc1e421ffc52e68
PAVEL <pasha_kondr@mail.ru>
2023-11-28 21:28:30 +1000

    feat: added main menu keyboard

--- handlers/menu_handlers.py
+++ handlers/menu_handlers.py
@@ -2,7 +2,7 @@ from aiogram import F, Router
 from aiogram.types import Message
 from aiogram.filters import Command, CommandStart
 from database.database import users_db
-from keyboards.keyboards import yes_no_kb
+from keyboards.keyboards import yes_no_kb, menu_kb
 from lexicon.lexicon_ru import LEXICON_RU
 
 
@@ -14,8 +14,10 @@ router = Router()
 @router.message(CommandStart())
 async def start_command(message: Message):
     if message.from_user.id in users_db:  # Если пользователь уже в базе
-        await message.answer(text=f'Приветствую тебя, {users_db[message.from_user.id]}, я Небула - бот Молекулы')
-        #  Вывести пользователю кнопки меню
+        await message.answer(text=f'Приветствую тебя, {users_db[message.from_user.id]}, я Небула - бот Молекулы\n\n' 
+                                  f'{LEXICON_RU["text_menu"]}', 
+                                  reply_markup=menu_kb)
+        
     elif message.from_user.full_name:
         await message.answer(text=f'Приветствую тебя, {message.from_user.full_name}, я Небула - бот Молекулы\n\n'
                                    'Могу ли я к тебе так обращаться?', reply_markup=yes_no_kb)
--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -10,3 +10,15 @@ yes_no_kb = ReplyKeyboardMarkup(
     keyboard=[[yes, no]],
     resize_keyboard=True,
     one_time_keyboard=True)
+
+# Клавиатура главного меню
+events_button = KeyboardButton(text=LEXICON_RU['events'])
+rent_button = KeyboardButton(text=LEXICON_RU['rent'])
+pictures_button = KeyboardButton(text=LEXICON_RU['pictures'])
+project_button = KeyboardButton(text=LEXICON_RU['project'])
+
+menu_builder = ReplyKeyboardBuilder()
+
+menu_builder.row(events_button, rent_button, pictures_button, project_button, width=1)
+
+menu_kb: ReplyKeyboardMarkup = menu_builder.as_markup(one_time_keyboard=True, resize_keyboard=True)
