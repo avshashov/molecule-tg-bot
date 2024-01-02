9fa7859d1ae5dd88b3ba03e69442216c281ef5b5
avshashov <tauquantum@gmail.com>
2024-01-02 21:43:43 +1000

    feat: добавлены константы для медиа блоков и типов

--- app/constants.py
+++ app/constants.py
@@ -5,3 +5,15 @@ class PictureStatus(Enum):
     READY = 'ready'
     ORDER = 'order'
     RENT = 'rent_order'
+
+
+class MediaBlock(int, Enum):
+    RENT = 1
+    PICTURE = 2
+    ABOUT_PROJECT = 3
+
+
+class MediaType(int, Enum):
+    PHOTO = 1
+    PRESENTATION = 2
+
