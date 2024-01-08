a4a3e9c1073be62f138fcc0ccb9d290aedd1f28b
avshashov <tauquantum@gmail.com>
2024-01-08 14:10:47 +1000

    fix: исправлены функции формирования сообщений

--- app/text_creator.py
+++ app/text_creator.py
@@ -7,53 +7,66 @@ from app.database.crud import CRUDUser
 # формирование сообшений
 class TextCreator:
     @staticmethod
-    def create_text_ready(users_db, user_id, mode: str, **kwargs) -> str:
-        text_name = f'Имя: {users_db[user_id]["name"]}\n'
-        text_contact = (
-            f'Телефон: {kwargs.get("enter_telephone", "-")}\n'
-            f'Способ связи: {kwargs.get("how_contact", "-")}\n'
-        )
-        text_email = f'Email: {kwargs.get("enter_email", "-")}\n'
+    async def create_text_ready(
+        session: AsyncSession, user_id: int, mode: str, **kwargs
+    ) -> str:
+        text = ''
         if mode == PictureStatus.READY:
-            text_ready = f'Заказ: Готовая картина\n\n' + text_name
+            full_name, username = await TextCreator._get_username_and_full_name(
+                session, user_id
+            )
+            text += (
+                f'Заказ: Готовая картина\n\n'
+                f'Имя: {full_name}'
+                f'\nКонтакт: @{username}\n'
+            )
             if 'enter_telephone' in kwargs:
-                finish_text = text_ready + text_contact
+                text += (
+                    f'Телефон: {kwargs.get("enter_telephone", "-")}\n'
+                    f'Способ связи: {kwargs.get("how_contact", "-")}\n'
+                )
             elif 'enter_email' in kwargs:
-                finish_text = text_ready + text_email
-        return finish_text
+                text += f'Email: {kwargs.get("enter_email", "-")}\n'
+        return text
 
     @staticmethod
-    def create_text_order(users_db, user_id, mode: str, **kwargs) -> str:
+    async def create_text_order(
+        session: AsyncSession, user_id: int, mode: str, **kwargs
+    ) -> str:
+        text = ''
         if mode == PictureStatus.ORDER:
-            text_name = f'Имя: {users_db[user_id]["name"]}\n'
-            text_contact = (
-                f'Телефон: {kwargs.get("enter_telephone", "-")}\n'
-                f'Способ связи: {kwargs.get("how_contact", "-")}\n'
+            full_name, username = await TextCreator._get_username_and_full_name(
+                session, user_id
             )
-            text_email = f'Email: {kwargs.get("enter_email", "-")}\n'
-            text_order = f'Заказ картины\n\n' + text_name
-            text_general = (
+            text += (
+                f'Заказ картины\n\n' f'Имя: {full_name}\n' f'\nКонтакт: @{username}\n'
+            )
+            if 'enter_telephone' in kwargs:
+                text += (
+                    f'Телефон: {kwargs.get("enter_telephone", "-")}\n'
+                    f'Способ связи: {kwargs.get("how_contact", "-")}\n'
+                )
+            elif 'enter_email' in kwargs:
+                text += f'Email: {kwargs.get("enter_email", "-")}\n'
+            text += (
                 f'Кому картина: {kwargs.get("for_whom", "   ---")}\n'
                 f'Событие: {kwargs.get("event", "   ---")}\n'
                 f'Размер: {kwargs.get("size", "   ---")}\n'
                 f'Настроение: {kwargs.get("mood", "   ---")}\n'
                 f'Цвета: {kwargs.get("color", "   ---")}'
             )
-            if 'enter_telephone' in kwargs:
-                finish_text = text_order + text_contact + text_general
-            elif 'enter_email' in kwargs:
-                finish_text = text_order + text_email + text_general
-        return finish_text
+        return text
 
     @staticmethod
     async def create_text_rent(
         session: AsyncSession, user_id: int, mode: str, **kwargs
     ) -> str:
+        text = ''
         if mode == PictureStatus.RENT:
             full_name, username = await TextCreator._get_username_and_full_name(
                 session, user_id
             )
-            text = (
+            text += (
                 f'Заказ: Аренда помещения\n\n'
                 f'Имя: {full_name}\n'
                 f'Контакт: @{username}\n'
